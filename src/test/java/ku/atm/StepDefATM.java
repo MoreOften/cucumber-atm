4e1f44bef4d2c1fa4663a6fd18574f4a0ab062d1
MoreOften <nonratchapat@gmail.com>
2025-08-24 14:24:44 +0700

    fix grammar typo

--- src/test/java/ku/atm/StepDefATM.java
+++ src/test/java/ku/atm/StepDefATM.java
@@ -50,7 +50,7 @@ public class StepDefATM {
     }
 
     @When("I deposit {float} to ATM")
-    public void i_deposit_from_atm(double amount) {  atm.deposit(amount); }
+    public void i_deposit_to_atm(double amount) {  atm.deposit(amount); }
 
     @When("I overdraw {float} from ATM")
     public void i_withdraw_from_atm_more_than_balance(double amount) throws NotEnoughBalanceException {
